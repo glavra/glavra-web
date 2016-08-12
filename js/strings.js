e48922b039fbb102474249b4aa57e4855258c399
KeyboardFire <andy@keyboardfire.com>
2016-08-11 19:17:54 -0500

    Implement privileges for message deletion

--- js/strings.js
+++ js/strings.js
@@ -14,7 +14,6 @@ var strings = {
         'The room ID that was specified was invalid.',
         'The room ID that was specified does not exist.',
         'Your username may not be more than 20 characters long.',
-        'You can\'t vote on your own messages.',
         'You\'re doing that too fast.'
     ],
     fmttime: function(date, relative) {
