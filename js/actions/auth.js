56bdb38afc86fbb71ae2f6f21ea09c533d10e8f9
KeyboardFire <andy@keyboardfire.com>
2016-08-13 15:06:46 -0500

    Fix bug when storing auth tokens

--- js/actions/auth.js
+++ js/actions/auth.js
@@ -1,7 +1,7 @@
 actions.auth = function(data) {
     if (data.success) {
         document.getElementById('account').innerText = 'logged in';
-        localStorage.setItem('glavra-token', data.token);
+        if (data.token) localStorage.setItem('glavra-token', data.token);
         dialog.showText(strings.authSuccess);
     } else {
         dialog.showText(strings.authFailure);
--- js/actions/register.js
+++ js/actions/register.js
@@ -1,7 +1,7 @@
 actions.register = function(data) {
     if (data.success) {
         document.getElementById('account').innerText = 'logged in';
-        localStorage.setItem('glavra-token', data.token);
+        if (data.token) localStorage.setItem('glavra-token', data.token);
         dialog.showText(strings.registerSuccess);
     } else {
         dialog.showText(strings.registerFailure);
