f9109c4eccfa55cca8d8bf30743b30c1181c26de
KeyboardFire <andy@keyboardfire.com>
2016-09-17 09:37:01 -0500

    Add login support on rooms page

--- js/rooms.js
+++ js/rooms.js
@@ -2,6 +2,28 @@ var actions = {}, util = {}, onload = [];
 
 window.addEventListener('load', function() {
 
+    var token = localStorage.getItem('glavra-token');
+    var sock = new WebSocket('ws://127.0.0.1:3012' +
+            (token ? '?token=' + token : ''));
+
+    sock.addEventListener('open', function() {
+        var loginLink = document.createElement('a');
+        loginLink.id = 'account';
+        loginLink.textContent = 'log in';
+        loginLink.href = 'javascript:;';
+        loginLink.addEventListener('click', util.loginListener = function(e) {
+            e.preventDefault();
+            dialog.showLoginPrompt(sock);
+        });
+        document.getElementById('links').appendChild(loginLink);
+    });
+
+    sock.addEventListener('message', function(e) {
+        var data = JSON.parse(e.data);
+        console.log(data);
+        actions[data.type](data);
+    });
+
     onload.forEach(function(f) { f(); });
 
 });
