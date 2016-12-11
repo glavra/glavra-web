7a90c8cdc1d0d55fa7292c10b3542d205cb6ec16
KeyboardFire <andy@keyboardfire.com>
2016-12-11 10:01:28 -0600

    Fix newly introduced bug
    I'm a dumb

--- js/chat.js
+++ js/chat.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function() {
     if (roomMatch = location.search.slice(1).match(/(^|&)room=(\d+)/)) {
         roomId = +roomMatch[2];
     } else if (roomMatch = location.pathname.match(/^\/chat\/(\d+)$/)) {
-        roomId = +roomMatch[2];
+        roomId = +roomMatch[1];
     } else return; // TODO room list?
 
     var token = localStorage.getItem('glavra-token');
